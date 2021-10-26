4911de0f72e8b22f46902c9922655685e4ec6842
avamay <avamay481516@gmail.com>
2021-10-26 12:57:51 +0300

    your message

---
+++ js/script.js
@@ -0,0 +1,41 @@
+//
+//
+// onload = function() {
+//   toggleSlider();
+// }
+//
+// window.onresize = function() {
+//   toggleSlider();
+// }
+//
+function toggleSlider() {
+//   var width = window.innerWidth;
+//   console.log(width);
+//
+//   if(width < 768) { //mobile
+//     $(".slider").show();
+//     $(".cards").hide();
+//   }
+//   else { //desktop
+//     $(".slider").hide();
+//     $(".cards").show();
+//   }
+// }
+if (window.matchMedia("(max-width: 768px)").matches){
+$(".slider").not('.slick-initialized').slick({
+
+  dots: false,
+  arrows: false,
+  slidesToShow: 2,
+  variableWidth: true,
+    infinite: false,
+});
+};
+}
+
+$(function(){
+    toggleSlider();//calling function on window load
+    $(window).resize(function(){
+        toggleSlider();//calling function on window resize
+    });
+});
